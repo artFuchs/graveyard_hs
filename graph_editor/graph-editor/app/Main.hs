d25ad426a7dc2b41c869b2b0b009c66aa94014bb
arthur fuchs <alfuchs17@gmail.com>
2019-01-06 18:42:46 -0200

    refactored some code, adding a application state struct and a helper module

--- graph_editor/graph-editor/app/Main.hs
+++ graph_editor/graph-editor/app/Main.hs
@@ -8,6 +8,27 @@ import qualified Data.Text as T
 import Data.List
 import Graph
 import GraphicalInfo
+import Helper
+
+-- | estado do editor de grafos
+type EditorState = (Graph, [Node], [Edge])
+
+editorGetGraph :: EditorState -> Graph
+editorGetGraph (g,_,_) = g
+
+editorGetSelected :: EditorState -> ([Node], [Edge])
+editorGetSelected (_,n,e) = (n,e)
+
+editorGetSelectedNodes :: EditorState -> [Node]
+editorGetSelectedNodes (_,n,_) = n
+
+editorGetSelectedEdges :: EditorState -> [Edge]
+editorGetSelectedEdges (_,_,e) = e
+
+
+
+
+
 
 main :: IO()
 main = do
@@ -77,16 +98,16 @@ main = do
   widgetShowAll window
 
   -- inicializa estado
-  -- (Graph, nodos selecionados)
-  st <- newIORef (graph1, [])
+  -- (Graph, nodos selecionados, arestas selecionadas)
+  st <- newIORef (graph1, [], [])
   oldPoint <- newIORef (0.0,0.0)
 
   -- TRATAMENTO DE EVENTOS -----------------------------------------------------
   -- tratamento de eventos - canvas --------------------------------------------
   -- evento de desenho
   canvas `on` draw $ do
-    (graph, selected) <- liftIO $ readIORef st
-    drawGraph graph selected canvas
+    es <- liftIO $ readIORef st
+    drawGraph es canvas
 
   -- clique do mouse
   canvas `on` buttonPressEvent $ do
@@ -97,24 +118,35 @@ main = do
       writeIORef oldPoint (x,y)
       widgetGrabFocus canvas
     case b of
+      -- clique com o botão esquerdo: seleciona nodos
       LeftButton  -> liftIO $ do
-        (_,selectedNodes) <- readIORef st
+        es <- readIORef st
+        let oldSN = editorGetSelectedNodes es
         checkSelect st (x,y) canvas
+        -- Shift: seleção de multiplos elementos
         if Shift `elem` ms
           then do
-            (graph,selected) <- readIORef st
-            let desselect = filter (\n -> n `elem` selectedNodes) selected
-                newSelected = filter (\n -> n `notElem` desselect) $ selectedNodes ++ selected
-            writeIORef st (graph, (newSelected))
+            es <- readIORef st
+            let graph = editorGetGraph es
+                sN = editorGetSelectedNodes es
+                desselect = filter (\n -> n `elem` oldSN) sN
+                jointSN = filter (\n -> n `notElem` desselect) $ oldSN ++ sN
+            writeIORef st (graph, jointSN, [])
           else return ()
         widgetQueueDraw canvas
-        (graph,selected) <- liftIO $ readIORef st
-        updatePropMenu selected entryNodeID entryNodeName colorBtn
+        es' <- liftIO $ readIORef st
+        let graph = editorGetGraph es'
+            sN = editorGetSelectedNodes es'
+        updatePropMenu sN entryNodeID entryNodeName colorBtn
         --labelSetText labelEdges' $ if (length selected == 1) then foldl (\s e -> s ++ (show e) ++ ", ") "" (getConnectors graph (selected!!0)) else ""
+      -- clique com o botão direito: insere edges entre nodos
       RightButton -> liftIO $ do
-        (_,selectedNodes) <- liftIO $ readIORef st
+        es <- liftIO $ readIORef st
         checkSelect st (x,y) canvas
-        (graph,newSelectedNode) <- liftIO $ readIORef st
+        es' <- liftIO $ readIORef st
+        let selectedNodes = editorGetSelectedNodes es
+            graph = editorGetGraph es'
+            newSelectedNode = editorGetSelectedNodes es'
         if Shift `elem` ms
           then do
             let nodeConnectors = if length newSelectedNode == 1
@@ -123,12 +155,12 @@ main = do
                 otherConnectors = concat . map (\n -> getConnectors graph n) $ selectedNodes
                 targetConnectors = filter (\e -> e `elem` nodeConnectors) otherConnectors
                 newGraph = foldl (\g e -> removeEdge g e) graph targetConnectors
-            writeIORef st (newGraph, newSelectedNode)
+            writeIORef st (newGraph, newSelectedNode, [])
           else do
             let newGraph = if length newSelectedNode == 1
                              then foldl (\g n -> insertEdge g n (newSelectedNode!!0)) graph selectedNodes
                              else graph
-            writeIORef st (newGraph, newSelectedNode)
+            writeIORef st (newGraph, newSelectedNode, [])
 
         widgetQueueDraw canvas
         updatePropMenu newSelectedNode entryNodeID entryNodeName colorBtn
@@ -186,6 +218,10 @@ main = do
 
   mainGUI
 
+
+
+-- Callbacks -------------------------------------------------------------------
+-- atualização do menu de propriedades -----------------------------------------
 updatePropMenu :: [Node] -> Entry -> Entry -> ColorButton-> IO()
 updatePropMenu selected entryID entryName colorBtn = do
   case length selected of
@@ -206,26 +242,26 @@ updatePropMenu selected entryID entryName colorBtn = do
       entrySetText entryID "--"
       entrySetText entryName "----"
 
-
-drawGraph :: Graph -> [Node] -> DrawingArea -> Render ()
-drawGraph g nodes canvas = do
+-- atualização do desenho do grafo --------------------------------------------
+drawGraph :: EditorState -> DrawingArea -> Render ()
+drawGraph state canvas = do
+  let (g, sNodes, _) = state
   context <- liftIO $ widgetGetPangoContext canvas
-  forM (graphGetNodes g) (\n -> renderNode n nodes context)
+  forM (graphGetNodes g) (\n -> renderNode n (n `elem`sNodes) context)
   forM (graphGetEdges g) (\e -> do
     let dstN = getDstNode g e
         srcN = getSrcNode g e
     case (srcN, dstN) of
-      (Just src, Just dst) -> renderEdge e src dst context
+      (Just src, Just dst) -> renderEdge e False src dst context
       (Nothing, _) -> return ()
       (_, Nothing) -> return ())
   return ()
 
 
 -- desenha um nodo, com seu texto
-renderNode :: Node -> [Node] -> PangoContext -> Render ()
-renderNode node selected_nodes context = do
-  let selected = find (\n -> n == node) selected_nodes  /= Nothing
-      (x,y) = position . infoGetGraphicalInfo . nodeGetInfo $ node
+renderNode :: Node -> Bool -> PangoContext -> Render ()
+renderNode node selected context = do
+  let (x,y) = position . infoGetGraphicalInfo . nodeGetInfo $ node
       (r,g,b) = if selected
                   then (0,0,1)
                   else color . infoGetGraphicalInfo . nodeGetInfo $ node
@@ -250,8 +286,9 @@ renderNode node selected_nodes context = do
   moveTo (x-(pw/2)) (y-(ph/2))
   showLayout pL
 
-renderEdge :: Edge -> Node -> Node -> PangoContext -> Render ()
-renderEdge edge nodeSrc nodeDst context = do
+-- desenha uma aresta
+renderEdge :: Edge -> Bool -> Node -> Node -> PangoContext -> Render ()
+renderEdge edge selected nodeSrc nodeDst context = do
   setSourceRGB 0 0 0
 
   -- utiliza a biblioteca Pango para calcular o tamanho da bounding box do texto
@@ -262,19 +299,23 @@ renderEdge edge nodeSrc nodeDst context = do
   (_,PangoRectangle px py pw ph) <- liftIO $ layoutGetExtents pL
   (_,PangoRectangle px2 py2 pw2 ph2) <- liftIO $ layoutGetExtents pL2
 
-  -- calcula os pontos de origem e destino da aresta
+  -- calculo dos pontos de origem e destino da aresta
   let (x1,y1) = position . infoGetGraphicalInfo . nodeGetInfo $ nodeSrc
       (x2,y2) = position . infoGetGraphicalInfo . nodeGetInfo $ nodeDst
       (xe, ye) = position . infoGetGraphicalInfo . edgeGetInfo $ edge
-      d = sqrt $ (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
-      (vx1,vy1) = ((xe-x1)/d , (ye-y1)/d)
-      (vx2,vy2) = ((x2-xe)/d , (y2-ye)/d)
+      d1 = pointDistance (x1,y1) (xe,ye)
+      d2 = pointDistance (xe,ye) (x2,y2)
+      (vx1,vy1) = ((xe-x1)/d1 , (ye-y1)/d1)
+      (vx2,vy2) = ((x2-xe)/d2 , (y2-ye)/d2)
       n1 = 3 + (max pw ph)/2
       n2 = 5 + (max pw2 ph2)/2
       (x1', y1') = (x1 + vx1*n1, y1 + vy1*n1)
       (x2', y2') = (x2 - vx2*n2, y2 - vy2*n2)
+      -- configurações de cor
+      (r,g,b) = if selected then (1,1,1) else lineColor . infoGetGraphicalInfo . edgeGetInfo $ edge
 
   -- desenha uma linha representando a aresta
+  setSourceRGB r g b
   moveTo x1' y1'
   lineTo xe  ye
   lineTo x2' y2'
@@ -285,10 +326,11 @@ renderEdge edge nodeSrc nodeDst context = do
   arc xe  ye 2 0 (2*pi)
   fill
 
--- verifica se o usuario selecionou algum nodo
-checkSelect:: IORef (Graph, [Node]) -> (Double,Double) -> DrawingArea -> IO ()
+-- verifica se o usuario selecionou algum nodo ---------------------------------
+checkSelect:: IORef EditorState -> (Double,Double) -> DrawingArea -> IO ()
 checkSelect state (x,y) canvas = do
-  (g,s) <- readIORef state
+  es <- readIORef state
+  let g = editorGetGraph es
   context <- widgetGetPangoContext canvas
   maybeSelectedNodes <- forM (graphGetNodes g) $ (\n -> do
     inside <- pointInsideNode n (x,y) context
@@ -300,23 +342,14 @@ checkSelect state (x,y) canvas = do
                                 Nothing -> False
                                 _ -> True) $ reverse maybeSelectedNodes
   case maybeSelected of
-    Just (Just a) -> writeIORef state (g,[a])
-    _ -> writeIORef state (g,[])
+    Just (Just a) -> writeIORef state (g,[a],[])
+    _ -> writeIORef state (g,[],[])
   return ()
 
---verifica se um ponto está dentro da bounding box de um nodo
---utiliza métodos da biblioteca Pango para isso
-pointInsideNode:: Node -> (Double,Double) -> PangoContext -> IO Bool
-pointInsideNode node (x,y) context = do
-  pL <- layoutText context $ infoGetContent . nodeGetInfo $ node
-  (_, PangoRectangle px py pw ph) <- layoutGetExtents pL
-  let (nx,ny) = position . infoGetGraphicalInfo . nodeGetInfo $ node
-  return $ (x >= nx - pw/2) && (x <= nx + pw/2) && (y >= ny - ph/2) && (y <= ny + ph/2)
-
--- move um nodo do grafo
-moveNode:: IORef (Graph, [Node]) -> (Double,Double) -> (Double,Double) -> IO ()
+-- move um nodo do grafo -------------------------------------------------------
+moveNode:: IORef EditorState -> (Double,Double) -> (Double,Double) -> IO ()
 moveNode state (xold,yold) (xnew,ynew) = do
-  (graph,nodes) <- readIORef state
+  (graph,nodes, edges) <- readIORef state
   let (deltaX, deltaY) = (xnew-xold, ynew-yold)
   maybeNodes <- forM (graphGetNodes graph) (\node -> do
     if node `elem` nodes
@@ -334,34 +367,40 @@ moveNode state (xold,yold) (xnew,ynew) = do
                                 Nothing -> acc)
       newGraph = foldl mvg graph maybeNodes
       newNodes = foldl rmMaybe [] maybeNodes
-  writeIORef state (newGraph, newNodes)
+  writeIORef state (newGraph, newNodes, edges)
 
+-- operações básicas sobre o grafo ---------------------------------------------
 -- cria um novo nodo e insere no grafo
-createNode:: IORef (Graph, [Node]) -> (Double,Double) -> IO()
+createNode:: IORef EditorState -> (Double,Double) -> IO()
 createNode state pos = do
-  (graph, nodes) <- readIORef state
+  (graph, nodes, _) <- readIORef state
   let maxnode = if length (graphGetNodes graph) > 0
                   then maximum (graphGetNodes graph)
                   else Node 0 $ Info "" newGraphicalInfo
       newID = 1 + nodeGetID maxnode
       newNode = Node newID $ Info ("node " ++ show newID) $ giSetPosition newGraphicalInfo pos
       newGraph = insertNode graph newNode
-  writeIORef state (newGraph, [newNode])
+  writeIORef state (newGraph, [newNode], [])
 
--- deleta os nodos selecionados do grafo
-deleteNode:: IORef (Graph, [Node]) -> IO()
+-- deleta os nodos selecionados no grafo
+deleteNode:: IORef EditorState -> IO()
 deleteNode state = do
-  (graph, nodes) <- readIORef state
+  (graph, nodes, _) <- readIORef state
   let newGraph = foldl (\g n -> removeNode g n) graph nodes
-  writeIORef state (newGraph, [])
+  writeIORef state (newGraph, [], [])
 
+-- renomeia os nodos selecionados
+renameNode:: IORef EditorState -> String -> IO()
 renameNode state name = do
-  (graph, nodes) <- readIORef state
+  (graph, nodes, _) <- readIORef state
   let renamedNodes = map (\n -> Node (nodeGetID n) $ Info name (infoGetGraphicalInfo . nodeGetInfo $ n)) nodes
       newGraph = foldl (\g n -> changeNode g n) graph renamedNodes
-  writeIORef state (newGraph,renamedNodes)
+  writeIORef state (newGraph,renamedNodes, [])
 
 -- ↓↓↓↓↓ estruturas para teste ↓↓↓↓↓ -------------------------------------------
+
+graph1 :: Graph
+graph1 = Graph "1" src1 dst1 edges1 nodes1
 
 nodes1 :: [Node]
 nodes1 =  [(Node 1 $ Info "hello" $ giSetPosition newGraphicalInfo (100, 40))
@@ -372,18 +411,12 @@ nodes1 =  [(Node 1 $ Info "hello" $ giSetPosition newGraphicalInfo (100, 40))
           ]
 
 edges1 :: [Edge]
-edges1 =  [(Edge 1 $ Info "" $ giSetPosition newGraphicalInfo (70,70))
-          ,(Edge 2 $ Info "" $ giSetPosition newGraphicalInfo (130,70))
-          ,(Edge 3 $ Info "" $ giSetPosition newGraphicalInfo (40,130))
+edges1 =  [(Edge 1 $ Info "" $ giSetPosition newGraphicalInfo ( 70, 70))
+          ,(Edge 2 $ Info "" $ giSetPosition newGraphicalInfo (130, 70))
+          ,(Edge 3 $ Info "" $ giSetPosition newGraphicalInfo ( 40,130))
           ,(Edge 4 $ Info "" $ giSetPosition newGraphicalInfo (100,130))
           ,(Edge 5 $ Info "" $ giSetPosition newGraphicalInfo (100,160))
           ]
-
-graph1 :: Graph
-graph1 = Graph "1" src1 dst1 edges1 nodes1
-
-
--- ↓↓↓↓↓ src / dst funções para testar o grafo ↓↓↓↓↓ ---------------------------
 
 -- 1 -1-> 2
 -- 1 -2-> 3
