759b5f73f59b193e38b20de1b8cbae7ec4e86c05
artFuchs <alfuchs17@gmail.com>
2019-04-03 17:07:33 -0300

    testing with builder to replace the uiManager in the creation of the menubar because GtkUiManager is deprecated

---
+++ graphics/test-ui-builders/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for test-ui-builders
+
+## Unreleased changes
---
+++ graphics/test-ui-builders/app/Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Graphics.UI.Gtk
+import Control.Monad
+import Control.Monad.IO.Class
+
+main = do
+  initGUI
+
+  -- create window
+  window <- windowNew
+  set window [ windowTitle        := "hohohoho"
+             , windowDefaultWidth := 300
+             , windowDefaultHeight := 200
+             ]
+
+  vboxMain <- vBoxNew False 8
+  containerAdd window vboxMain
+
+  -- load the menubar
+  builder <- builderNew
+  builderAddFromFile builder "./menubar.ui"
+  menubar <- builderGetObject builder castToWidget "menubar1"
+  boxPackStart vboxMain menubar PackNatural 0
+
+  newItem <- builderGetObject builder castToMenuItem "new_item"
+  newItem `on` menuItemActivated $ putStrLn "new file"
+
+  saveItem <- builderGetObject builder castToMenuItem "save_item"
+  saveItem `on` menuItemActivated $ putStrLn "save file"
+
+  openItem <- builderGetObject builder castToMenuItem "open_item"
+  openItem `on` menuItemActivated $ putStrLn "open file"
+
+
+
+  window `on` deleteEvent $ do
+    liftIO $ mainQuit
+    return False
+
+  widgetShowAll window
+  mainGUI
